27c9b2661e2bcb0631db36b7f2191ff084d9122c
aetuk <webandserver@gmail.com>
2012-12-02 13:25:59 +0000

    Init versions

---
+++ wasjsapi_template.js
@@ -0,0 +1,11 @@
+/* This website and its content is copyright of Web and Server Solution - © Web and Server Solution 2012. All rights reserved.
+Any redistribution or reproduction of part or all of the contents in any form is prohibited.
+You may not, except with our express written permission, distribute or commercially exploit 
+the content. Nor may you transmit it or store it in any other website or other 
+form of electronic retrieval system.
+ */
+
+(function() {
+
+
+})();
